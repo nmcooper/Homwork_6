5b20d9e8d968281a61b0d067ca589967b120fc88
nmcooper <nmcooper@ucsc.edu>
2014-05-17 09:50:19 -0700

    Version 5

--- Nathaniel_Cooper_hwk_6.m
+++ Nathaniel_Cooper_hwk_6.m
@@ -13,7 +13,7 @@ K=15000; % carring capacity (1st value)
 n0=[8000;2000]; % Vi=8000 and Pi=2000 inital values
 
 % ODE solver 
-[T, Y] = ode45(@(t,y) LV_Pred_RM(y,b,a,e,d,K,h),time,n0); % solves de
+[T, Y] = ode45(@(t,y) LV_Pred_RM(y,b,a,e,d,K,h),time,n0); % SEE ATTACHED FUNCTION
 
 figure;
 subplot(1,2,1);
@@ -26,10 +26,41 @@ subplot(1,2,2);
 isoV = @(x) b/a + (b.*x.*(a*h -1/K - (a/K).*h.*x))./a  ; % Prey isocline
 plot(1:25,feval(isoV,1:25))
 isoP = d/(e*a - a*d*h); % predator isocline
-line([isoP isoP],[0 100],'Color','g');
+line([isoP isoP],[0 10000],'Color','g');
 plot(Y(:,1),Y(:,2),'r-','LineWidth',2)
 xlabel('V, prey abundance')
 ylabel('P, predator abundance')
 legend({'Prey isocline','Predator isocline','Population trajectory'});  
 ylim([0 10000]); xlim([0 25])
 hold off
+
+
+%% Qusetion 2
+% set matrix paramators for Jacobian
+
+V=d/(a*e-a*d*h); % V at equalibrium 
+P=(b/a)+(b*V(a*h-(1/K)-(a/K)*h*V)/a; % P at equlibrium
+
+a11=b-(P*a)/(V*a*h+1)^2-(2*V*b)/K; % p(dv/dv)
+
+a12=-(V*a)/(V*a*h+1); % P(dv/dp)
+
+a21=(P*a*e)/(V*a*h+1)^2; %p(dp/dv)
+
+a22=(V*a*e)/(V*a*h+1)-d; % P(dp/dv)
+
+J=[a11  a12; a21  a22]; % Jacobian Matrix 
+
+J_eig=eig(J); % egien valuse of Jacobian matrix
+
+
+
+
+
+
+
+
+
+
+
+
