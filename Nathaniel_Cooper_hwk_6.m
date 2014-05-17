cf9efe6b504181e073600cc4e4b033391cd2ae9c
nmcooper <nmcooper@ucsc.edu>
2014-05-17 07:10:35 -0700

    version 1

---
+++ Nathaniel_Cooper_hwk_6.m
@@ -0,0 +1,9 @@
+%% Nathaniel Cooper Hwk 6
+% Qustion 1
+
+% Parameters
+a=0.001; % Attact rate
+e=0.1; % prey to preditor coverstion efficieny
+d=0.1; %predator birth rate
+b=0.5; % prey birth rate
+h=0.0015; % prey handiling time
