7a72168c25c94f130be79c73598d7a94c0cb3707
nmcooper <nmcooper@ucsc.edu>
2014-05-17 07:25:06 -0700

    Version 2

--- Nathaniel_Cooper_hwk_6.m
+++ Nathaniel_Cooper_hwk_6.m
@@ -2,8 +2,15 @@
 % Qustion 1
 
 % Parameters
+time=1:25; % 25 year simulation
 a=0.001; % Attact rate
 e=0.1; % prey to preditor coverstion efficieny
 d=0.1; %predator birth rate
 b=0.5; % prey birth rate
 h=0.0015; % prey handiling time
+k=15000; % carring capacity (1st value) 
+
+n0=[8000;2000]; % Vi=8000 and Pi=2000 inital values
+
+% ODE solver 
+[T, Y] = ode45(@(t,y) LV_Pred_RM(y,b,a,e,d,K,h),time,n0);
